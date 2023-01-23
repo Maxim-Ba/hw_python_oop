73bff60c934a386e283cda21a39e8c31cd0b679a
Maxim Balashov <79164211428@yandex.ru>
2023-01-23 23:01:51 +0300

    Fix code after code review

--- homework.py
+++ homework.py
@@ -85,7 +85,12 @@ class Training:
         calories = self.get_spent_calories()
 
         return InfoMessage(
-            type(self).__name__, self.duration, distance, speed, calories
+            # type(self).__name__, self.duration, distance, speed, calories
+            self.__class__.__name__,
+            self.duration,
+            distance,
+            speed,
+            calories,
         )
 
 
